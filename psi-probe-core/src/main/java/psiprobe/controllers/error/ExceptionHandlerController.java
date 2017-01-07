2f58f8a50af9ed5df4eb2a7729fa790fc8a1747b
Jeremy Landis <jeremylandis@hotmail.com>
2017-01-06 22:49:38 -0500

    [config] Add exception handler controller

---
+++ psi-probe-core/src/main/java/psiprobe/controllers/error/ExceptionHandlerController.java
@@ -0,0 +1,24 @@
+/**
+ * Licensed under the GPL License. You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ */
+package psiprobe.controllers.error;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+  @ExceptionHandler(Exception.class)
+  public String handleException(Exception e) {
+    return "/WEB-INF/jsp/errors/servleterror.jsp";
+  }
+
+}
