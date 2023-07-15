6e85580a6afe7ba34a7e6b003bae4540dbc604dd
Jeremy Landis <jeremylandis@hotmail.com>
2023-07-15 19:15:57 -0400

    webjars - use classpath

--- psi-probe-core/src/main/java/psiprobe/ProbeConfig.java
+++ psi-probe-core/src/main/java/psiprobe/ProbeConfig.java
@@ -365,7 +365,8 @@ public class ProbeConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     logger.debug("Registering webjars resource handler");
-    registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 
   /**
