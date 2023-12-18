c0326d88b6a437993af194f4ba2507882dcce034
Jeremy Landis <jeremylandis@hotmail.com>
2023-12-18 17:27:17 -0500

    WIP more on webjars

--- psi-probe-core/src/main/java/psiprobe/ProbeSecurityConfig.java
+++ psi-probe-core/src/main/java/psiprobe/ProbeSecurityConfig.java
@@ -61,8 +61,10 @@ public class ProbeSecurityConfig {
    */
   @Bean(name = "securityFilterChain")
   public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests().requestMatchers("/webjars/**").permitAll().anyRequest()
-        .authenticated().and().addFilter(securityContextHolderFilter(securityContextRepository())
+    http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/webjars/**"))
+        .permitAll();
+    http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+        .anyRequest().authenticated().and().addFilter(securityContextHolderFilter(securityContextRepository())
         .addFilter(getJ2eePreAuthenticatedProcessingFilter()).addFilter(getLogoutFilter())
         .addFilter(getExceptionTranslationFilter()).addFilter(getAuthorizationFilter());
     return http.build();
