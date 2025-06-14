a8ac87f0803acf9d728de3bef39c96155faa2db0
Jeremy Landis <jeremylandis@hotmail.com>
2025-06-14 19:04:00 -0400

    On security context, set allow session creation to false

--- psi-probe-core/src/main/java/psiprobe/ProbeSecurityConfig.java
+++ psi-probe-core/src/main/java/psiprobe/ProbeSecurityConfig.java
@@ -101,7 +101,9 @@ public class ProbeSecurityConfig {
    */
   @Bean
   public SecurityContextRepository securityContextRepository() {
-    return new HttpSessionSecurityContextRepository();
+    HttpSessionSecurityContextRepository repo = new HttpSessionSecurityContextRepository();
+    repo.setAllowSessionCreation(false);
+    return repo;
   }
 
   @Bean
